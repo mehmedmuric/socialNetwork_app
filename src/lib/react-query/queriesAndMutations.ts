f8435dbe737f9779d6d13fbb41b1690c0518da0d
Mehmed Muric <mehmedmuric22@gmail.com>
2024-02-20 01:05:40 +0100

    creating post

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -5,7 +5,8 @@ import {
   useInfiniteQuery
 } from "@tanstack/react-query";
 import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import { INewPost, INewUser } from "@/types";
+import { CreatePost } from "@/_root/pages";
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -28,3 +29,16 @@ export const useSignOutAccount = () => {
         mutationFn: signOutAccount
     });
 };
+
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (post: INewPost) => CreatePost(post),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+      },
+    });
+  };
